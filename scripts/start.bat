83e893cf61d62371f9f3d3a79d2ace0d5dff09bb
rubyu <tukigase@gmail.com>
2024-11-10 11:28:37 +0900

    add some scripts for downloading a model, starting the program

---
+++ scripts/start.bat
@@ -0,0 +1,9 @@
+set PATH=%PATH%;C:\Program Files\NVIDIA GPU Computing Toolkit\cudnn-windows-x86_64-8.9.7.29_cuda12-archive\bin
+set PATH=%PATH%;C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.6\bin
+
+cd ..
+call .venv\Scripts\activate
+
+python whisper_online_server.py --host 0.0.0.0 --port 43007 --task transcribe --model large-v3 --language en --backend faster-whisper --min-chunk-size 1
+
+deactivate
